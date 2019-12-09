ae4be0395e9caf21f02ae99da77ddb45559c8849
Kevin0M16 <kevin@nmscoordinates.com>
2019-12-08 18:17:16 -0600

    v1.1.10 Update #2
    Fixed Game mode not found.
    Galaxy type and name are now shown next to combobox on manual travel tab.
    Fixed a rare issue when NMS isn't installed in normal dir

--- NMSCoordinates/Form1.cs
+++ NMSCoordinates/Form1.cs
@@ -36,7 +36,7 @@ namespace NMSCoordinates
             InitializeComponent();
 
             //Set Version here
-            Version = "v1.1.9";
+            Version = "v1.1.10";
             label29.Text = "Version " + Version;
 
             Glyphs();
@@ -638,6 +638,8 @@ namespace NMSCoordinates
 
             comboBox3.Items.Clear();
             pgalaxy = "";
+            label35.Text = "Galaxy Name";
+            label36.Text = "TYPE";
 
             galaxy = "";
             X = "";
@@ -731,6 +733,10 @@ namespace NMSCoordinates
             gameMode.Add(new KeyValuePair<string, string>("6676", "Permadeath"));
             //gameMode.Add(new KeyValuePair<string, string>("", "Creative"));
 
+            gameMode.Add(new KeyValuePair<string, string>("4634", "Normal"));
+            gameMode.Add(new KeyValuePair<string, string>("5658", "Survival"));
+            gameMode.Add(new KeyValuePair<string, string>("6682", "Permadeath"));
+            gameMode.Add(new KeyValuePair<string, string>("5146", "Creative"));
         }
         private void GIndex()
         {
@@ -762,8 +768,99 @@ namespace NMSCoordinates
             galaxyDict.Add(new KeyValuePair<string, string>("23", "Yeberhahne"));
             galaxyDict.Add(new KeyValuePair<string, string>("24", "Twerbetek"));
             galaxyDict.Add(new KeyValuePair<string, string>("25", "Sivarates"));
+            galaxyDict.Add(new KeyValuePair<string, string>("26", "Eajerandal"));
+            galaxyDict.Add(new KeyValuePair<string, string>("27", "Aldukesci"));
+            galaxyDict.Add(new KeyValuePair<string, string>("28", "Wotyarogii"));
+            galaxyDict.Add(new KeyValuePair<string, string>("29", "Sudzerbal"));
+            galaxyDict.Add(new KeyValuePair<string, string>("30", "Maupenzhay"));
+            galaxyDict.Add(new KeyValuePair<string, string>("31", "Sugueziume"));
+            galaxyDict.Add(new KeyValuePair<string, string>("32", "Brogoweldian"));
+            galaxyDict.Add(new KeyValuePair<string, string>("33", "Ehbogdenbu"));
+            galaxyDict.Add(new KeyValuePair<string, string>("34", "Ijsenufryos"));
+            galaxyDict.Add(new KeyValuePair<string, string>("35", "Nipikulha"));
+            galaxyDict.Add(new KeyValuePair<string, string>("36", "Autsurabin"));
+            galaxyDict.Add(new KeyValuePair<string, string>("37", "Lusontrygiamh"));
+            galaxyDict.Add(new KeyValuePair<string, string>("38", "Rewmanawa"));
+            galaxyDict.Add(new KeyValuePair<string, string>("39", "Ethiophodhe"));
+            galaxyDict.Add(new KeyValuePair<string, string>("40", "Urastrykle"));
+            galaxyDict.Add(new KeyValuePair<string, string>("41", "Xobeurindj"));
+            galaxyDict.Add(new KeyValuePair<string, string>("42", "Oniijialdu"));
+            galaxyDict.Add(new KeyValuePair<string, string>("43", "Wucetosucc"));
+            galaxyDict.Add(new KeyValuePair<string, string>("44", "Ebyeloof"));
+            galaxyDict.Add(new KeyValuePair<string, string>("45", "Odyavanta"));
+            galaxyDict.Add(new KeyValuePair<string, string>("46", "Milekistri"));
+            galaxyDict.Add(new KeyValuePair<string, string>("47", "Waferganh"));
+            galaxyDict.Add(new KeyValuePair<string, string>("48", "Agnusopwit"));
+            galaxyDict.Add(new KeyValuePair<string, string>("49", "Teyaypilny"));
+            galaxyDict.Add(new KeyValuePair<string, string>("50", "Zalienkosm"));
+            galaxyDict.Add(new KeyValuePair<string, string>("51", "Ladgudiraf"));
+            galaxyDict.Add(new KeyValuePair<string, string>("52", "Mushonponte"));
+            galaxyDict.Add(new KeyValuePair<string, string>("53", "Amsentisz"));
+            galaxyDict.Add(new KeyValuePair<string, string>("54", "Fladiselm"));
+            galaxyDict.Add(new KeyValuePair<string, string>("55", "Laanawemb"));
+            galaxyDict.Add(new KeyValuePair<string, string>("56", "Ilkerloor"));
+            galaxyDict.Add(new KeyValuePair<string, string>("57", "Davanossi"));
+            galaxyDict.Add(new KeyValuePair<string, string>("58", "Ploehrliou"));
+            galaxyDict.Add(new KeyValuePair<string, string>("59", "Corpinyaya"));
+            galaxyDict.Add(new KeyValuePair<string, string>("60", "Leckandmeram"));
+            galaxyDict.Add(new KeyValuePair<string, string>("61", "Quulngais"));
+            galaxyDict.Add(new KeyValuePair<string, string>("62", "Nokokipsechl"));
+            galaxyDict.Add(new KeyValuePair<string, string>("63", "Rinblodesa"));
+            galaxyDict.Add(new KeyValuePair<string, string>("64", "Loydporpen"));
+            galaxyDict.Add(new KeyValuePair<string, string>("65", "Ibtrevskip"));
+            galaxyDict.Add(new KeyValuePair<string, string>("66", "Elkowaldb"));
+            galaxyDict.Add(new KeyValuePair<string, string>("67", "Heholhofsko"));
+            galaxyDict.Add(new KeyValuePair<string, string>("68", "Yebrilowisod"));
+            galaxyDict.Add(new KeyValuePair<string, string>("69", "Husalvangewi"));
+            galaxyDict.Add(new KeyValuePair<string, string>("70", "Ovna'uesed"));
+            galaxyDict.Add(new KeyValuePair<string, string>("71", "Bahibusey"));
+            galaxyDict.Add(new KeyValuePair<string, string>("72", "Nuybeliaure"));
+            galaxyDict.Add(new KeyValuePair<string, string>("73", "Doshawchuc"));
+            galaxyDict.Add(new KeyValuePair<string, string>("74", "Ruckinarkh"));
+            galaxyDict.Add(new KeyValuePair<string, string>("75", "Thorettac"));
+            galaxyDict.Add(new KeyValuePair<string, string>("76", "Nuponoparau"));
+            galaxyDict.Add(new KeyValuePair<string, string>("77", "Moglaschil"));
+            galaxyDict.Add(new KeyValuePair<string, string>("78", "Uiweupose"));
+            galaxyDict.Add(new KeyValuePair<string, string>("79", "Nasmilete"));
+            galaxyDict.Add(new KeyValuePair<string, string>("80", "Ekdaluskin"));
+            galaxyDict.Add(new KeyValuePair<string, string>("81", "Hakapanasy"));
+            galaxyDict.Add(new KeyValuePair<string, string>("82", "Dimonimba"));
+            galaxyDict.Add(new KeyValuePair<string, string>("83", "Cajaccari"));
+            galaxyDict.Add(new KeyValuePair<string, string>("84", "Olonerovo"));
+            galaxyDict.Add(new KeyValuePair<string, string>("85", "Umlanswick"));
+            galaxyDict.Add(new KeyValuePair<string, string>("86", "Henayliszm"));
+            galaxyDict.Add(new KeyValuePair<string, string>("87", "Utzenmate"));
+            galaxyDict.Add(new KeyValuePair<string, string>("88", "Umirpaiya"));
+            galaxyDict.Add(new KeyValuePair<string, string>("89", "Paholiang"));
+            galaxyDict.Add(new KeyValuePair<string, string>("90", "Iaereznika"));
+            galaxyDict.Add(new KeyValuePair<string, string>("91", "Yudukagath"));
+            galaxyDict.Add(new KeyValuePair<string, string>("92", "Boealalosnj"));
+            galaxyDict.Add(new KeyValuePair<string, string>("93", "Yaevarcko"));
+            galaxyDict.Add(new KeyValuePair<string, string>("94", "Coellosipp"));
+            galaxyDict.Add(new KeyValuePair<string, string>("95", "Wayndohalou"));
+            galaxyDict.Add(new KeyValuePair<string, string>("96", "Smoduraykl"));
+            galaxyDict.Add(new KeyValuePair<string, string>("97", "Apmaneessu"));
+            galaxyDict.Add(new KeyValuePair<string, string>("98", "Hicanpaav"));
+            galaxyDict.Add(new KeyValuePair<string, string>("99", "Akvasanta"));
+            galaxyDict.Add(new KeyValuePair<string, string>("100", "Tuychelisaor"));
+            galaxyDict.Add(new KeyValuePair<string, string>("109", "Nudquathsenfe"));
+            galaxyDict.Add(new KeyValuePair<string, string>("118", "Torweierf"));
+            galaxyDict.Add(new KeyValuePair<string, string>("129", "Broomerrai"));
+            galaxyDict.Add(new KeyValuePair<string, string>("138", "Emiekereks"));
+            galaxyDict.Add(new KeyValuePair<string, string>("149", "Zavainlani"));
+            galaxyDict.Add(new KeyValuePair<string, string>("158", "Rycempler"));
+            galaxyDict.Add(new KeyValuePair<string, string>("169", "Ezdaranit"));
+            galaxyDict.Add(new KeyValuePair<string, string>("178", "Wepaitvas"));
+            galaxyDict.Add(new KeyValuePair<string, string>("189", "Cugnatachh"));
+            galaxyDict.Add(new KeyValuePair<string, string>("198", "Horeroedsh"));
+            galaxyDict.Add(new KeyValuePair<string, string>("209", "Digarlewena"));
+            galaxyDict.Add(new KeyValuePair<string, string>("218", "Chmageaki"));
+            galaxyDict.Add(new KeyValuePair<string, string>("229", "Raldwicarn"));
+            galaxyDict.Add(new KeyValuePair<string, string>("238", "Yuwarugha"));
+            galaxyDict.Add(new KeyValuePair<string, string>("249", "Nepitzaspru"));
+            //galaxyDict.Add(new KeyValuePair<string, string>("", ""));
+
             galaxyDict.Add(new KeyValuePair<string, string>("140", "Kimycuristh"));
-            //galaxyDict.Add(new KeyValuePair<string, string>("24", "Twerbetek"));
         }
         private void Loadlsb1()
         {
@@ -848,6 +945,19 @@ namespace NMSCoordinates
             {
                 source.Text = mode;
                 AppendLine(textBox17, "Game mode Not Found, update needed.");
+            }
+        }
+        private void GalaxyLookupLbl(System.Windows.Forms.Label source, string galaxy)
+        {
+            //lookup the galaxy and if not in galaxy dict, display the number
+            try
+            {
+                source.Text = galaxyDict[galaxy];
+            }
+            catch
+            {
+                source.Text = "";//(Convert.ToInt32(galaxy) + 1).ToString();
+                //AppendLine(textBox17, "Galaxy Not Found, update needed.");
             }
         }
         private void GalaxyLookup(TextBox source, string galaxy)
@@ -2436,6 +2546,8 @@ namespace NMSCoordinates
             if (pgalaxy != null && pgalaxy != "")
             {
                 comboBox3.Items.Clear();
+                label35.Text = "";
+                label36.Text = "";
 
                 //Add Galaxy numbers
                 for (int i = 1; i <= 255; i++)
@@ -2445,15 +2557,73 @@ namespace NMSCoordinates
                 }
                 comboBox3.SelectedIndex = Convert.ToInt32(pgalaxy);
 
+                //Lookup and display galaxy name in label
+                GalaxyLookupLbl(label35, pgalaxy);
+                CheckLush();
+
                 //only sets the textbox to current location if manual travel is locked
-                if(unlockedToolStripMenuItem.Checked == false)
+                if (unlockedToolStripMenuItem.Checked == false)
                     textBox14.Text = textBox22.Text;
             }
         }
+        private void CheckLush()
+        {
+            var galaxy = Convert.ToInt32(pgalaxy);
+
+            var lushlist = new List<int>
+            {
+                9, 18, 29, 38, 49, 58, 69, 78, 89, 98, 109, 118,
+                129, 138, 149, 158, 169, 178, 189, 198, 209, 218,
+                229, 238, 249
+            };
+
+            var harshlist = new List<int>
+            {
+                2, 14, 22, 34, 42, 54, 62, 74, 82, 94, 102, 114, 122, 
+                134, 142, 154, 162, 174, 182, 194, 202, 214, 222, 234,
+                242, 254
+            };
+
+            var emptylist = new List<int>
+            {
+                6, 11, 26, 31, 46, 51, 66, 71, 86, 91, 106, 111, 126, 
+                131, 146, 151, 166, 171, 186, 191, 206, 211, 226, 231,
+                246, 251
+            };
+
+            if (lushlist.Contains(galaxy) || harshlist.Contains(galaxy) || emptylist.Contains(galaxy))
+            {
+                if (lushlist.Contains(galaxy))
+                {
+                    //label36.Visible = true;
+                    label36.Text = "LUSH";
+                }
+                if (harshlist.Contains(galaxy))
+                {
+                    //label36.Visible = true;
+                    label36.Text = "HARSH";
+                }
+                if (emptylist.Contains(galaxy))
+                {
+                    //label36.Visible = true;
+                    label36.Text = "EMPTY";
+                }
+            }
+            else
+            {
+                label36.Text = "NORMAL";
+                //label36.Visible = false;
+            }
+        }
+
         private void ComboBox3_SelectionChangeCommitted(object sender, EventArgs e)
         {
             //sets the player galaxy when combobox change committed to fast travel
             pgalaxy = comboBox3.SelectedIndex.ToString();
+
+            //Lookup and display galaxy name in label
+            GalaxyLookupLbl(label35, pgalaxy);
+            CheckLush();
         }
         private void ListBox4_MouseDoubleClick(object sender, MouseEventArgs e)
         {
